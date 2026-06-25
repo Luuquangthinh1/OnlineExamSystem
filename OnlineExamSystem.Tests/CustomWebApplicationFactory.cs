e0e0ba25e24ff8748c43ac65473ad7e6237b07a4
Lưu Quang Thịnh <153711498+Luuquangthinh1@users.noreply.github.com>
2026-06-25 10:15:33 +0700

    Set up in-memory database for testing
    Configured the test environment to use an in-memory database for the OnlineExamSystem tests.

--- OnlineExamSystem.Tests/CustomWebApplicationFactory.cs
+++ OnlineExamSystem.Tests/CustomWebApplicationFactory.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using OnlineExamSystem; // <-- namespace của web app
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineExamSystem.Data;
+using OnlineExamSystem;
 
 namespace OnlineExamSystem.Tests;
 
@@ -9,5 +12,29 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            // Remove SQL Server registration
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<OnlineExamSystemContext>));
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Use InMemory DB for tests
+            services.AddDbContext<OnlineExamSystemContext>(options =>
+            {
+                options.UseInMemoryDatabase("OnlineExamSystem_TestDb");
+            });
+
+            // Ensure DB is created
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<OnlineExamSystemContext>();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+        });
     }
 }
